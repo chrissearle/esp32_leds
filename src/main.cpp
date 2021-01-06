7d5ec651f7f9770d1c0673e52013595dcfe8a6b2
Chris Searle <chris@chrissearle.org>
2021-01-06 15:23:50 +0100

    Prepare drawPixels/colourFraction

--- src/main.cpp
+++ src/main.cpp
@@ -77,6 +77,37 @@ void updateScreen(unsigned int fps, const char *effect, int timeToChange)
   }
 }
 
+CRGB colourFraction(CRGB colourIn, float fraction)
+{
+  fraction = min(1.0f, fraction);
+  return CRGB(colourIn).fadeToBlackBy(255 * (1.0f - fraction));
+}
+
+void drawPixels(float position, float count, CRGB colour)
+{
+  float availFirstPixel = 1.0f - (position - (long)(position));
+  float amtFirstPixel = min(availFirstPixel, count);
+  float remaining = min(count, FastLED.size() - position);
+  int pos = position;
+
+  if (remaining > 0.0f)
+  {
+    FastLED.leds()[pos++] += colourFraction(colour, amtFirstPixel);
+    remaining -= amtFirstPixel;
+  }
+
+  while (remaining > 1.0f)
+  {
+    FastLED.leds()[pos++] += colour;
+    remaining--;
+  }
+
+  if (remaining > 0.0f)
+  {
+    FastLED.leds()[pos++] += colourFraction(colour, remaining);
+  }
+}
+
 void loop()
 {
   double effectDuration = 30.0;
